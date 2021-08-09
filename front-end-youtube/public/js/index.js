a33d6d152bfba1f854ae968d6a1e2b0c1ae48440
NguyenThanhAn201 <80932055+NguyenThanhAn201@users.noreply.github.com>
2021-08-09 16:28:53 +0700

    Add files via upload

---
+++ front-end-youtube/public/js/index.js
@@ -0,0 +1,8 @@
+var menuIcon = document.querySelector('.menu-icon');
+var sideBar = document.querySelector('.sidebar');
+var container = document.querySelector('.container');
+
+menuIcon.onclick = function() {
+    sideBar.classList.toggle('small-sidebar');
+    container.classList.toggle('large-container');
+}
